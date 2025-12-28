f7dcbff62c17b5bedf8f8ec39b74c8fa002db6a5
rizzla <roliwilhelm@gmail.com>
2025-12-28 14:20:04 -0500

    Set Vite base to / for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/meme-lab-site/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
